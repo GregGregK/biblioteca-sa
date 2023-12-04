a9a5d229bf424e899dd7e0cde176a51d38af655a
gregori.monteiro <gregori.monteiro@audaces.com>
2023-12-04 12:11:10 -0300

    "greg-final-commitv2"

--- biblioteca/src/main/resources/db/migration/V1__create-table-aluno.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE aluno (
-                       id SERIAL PRIMARY KEY,
-                       nome VARCHAR(255) NOT NULL,
-                       matricula BIGINT NOT NULL,
-                       created_at TIMESTAMP,
-                       status VARCHAR(255),
-                       contato VARCHAR(255)
-);
--- biblioteca/src/main/resources/db/migration/V1__create-table-livro.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE livro (
-                       id SERIAL PRIMARY KEY,
-                       title VARCHAR(255) NOT NULL,
-                       ISBN BIGINT,
-                       descricao TEXT,
-                       autor VARCHAR(255),
-                       disponibilidade VARCHAR(50)
-);
